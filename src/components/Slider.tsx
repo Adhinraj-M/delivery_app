f123b5ef77712392a890928b5804445a07410eed
Adhin Raj <adhinrajcr7@gmail.com>
2025-09-16 14:56:10 +0530

    slider section added

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -25,13 +25,13 @@ const Slider = () => {
 
     const [currentSlide,setCurrentSlide] = useState(0)
 
-    // useEffect(()=>{
-    //  const interval = setInterval(
-    //        ()=> setCurrentSlide(prev=>( prev +1) % data.length)
-    //  ,4000)
+    useEffect(()=>{
+     const interval = setInterval(
+           ()=> setCurrentSlide(prev=>( prev +1) % data.length)
+     ,4000)
 
-    //  return ()=> clearInterval(interval)
-    // },[])
+     return ()=> clearInterval(interval)
+    },[])
 
   return (
     <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-fuchsia-50'>
